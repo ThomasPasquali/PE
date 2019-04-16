8bcb3a3f7db73acbf829fcdfa2bd0e688608e645
ThomasPasquali <tom.pasqui00@gmail.com>
2019-04-16 17:02:19 +0200

    Runtime requests handler

--- temp/autofield.php
+++ temp/autofield.php
@@ -1,39 +1,39 @@
 <?php
-    include_once '..\controls.php';
-    $c = new Controls();
-    
-    if(!$c->logged()){
-        header('Location: /index.php?err=Utente non loggato');
-        exit();
-    }
-    
-    if($c->check(['action'], $_POST)){
-        switch ($_POST['action']) {
-            case 'hint':
-                switch ($_POST['type']) {
-                    case 'tecnico':
-                        $res = $c->db->ql(
-                            'SELECT ID, Cognome, Nome, Codice_fiscale 
+include_once '..\controls.php';
+$c = new Controls();
+
+if(!$c->logged()){
+    header('Location: /index.php?err=Utente non loggato');
+    exit();
+}
+
+if($c->check(['action'], $_POST)){
+    switch ($_POST['action']) {
+        case 'hint':
+            switch ($_POST['type']) {
+                case 'tecnico':
+                    $res = $c->db->ql(
+                    'SELECT ID, Cognome, Nome, Codice_fiscale
                              FROM tecnici
                              WHERE Cognome LIKE ? OR Nome LIKE ?
                               LIMIT 50',
-                             ["%$_POST[search]%", "%$_POST[search]%"]);
-                        header('Content-type: text/json');
-                        echo json_encode($res);
-                        exit();
+                              ["%$_POST[search]%", "%$_POST[search]%"]);
+                    header('Content-type: text/json');
+                    echo json_encode($res);
+                    exit();
                     break;
                     
-                    default:
-                        ;
+                default:
+                    ;
                     break;
-                }
+            }
             break;
             
-            default:
-                ;
+        default:
+            ;
             break;
-        }
     }
+}
     
 ?>
 <html>
