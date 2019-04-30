43e13e82270de10c1e7b27f71f08d521a02f475b
Thomas <Thomas@T420-Thomas>
2019-04-30 09:30:05 +0200

    Progressi gestione edifici

--- gestione/edifici.js
+++ gestione/edifici.js
@@ -1,3 +1,31 @@
+let f = getParameter("flag");
+if(f) changeContent(f);
+
+var mappaliNewEdCount = 1;
+var validNewEdSubmit = false;
+
 function reloadPageWithFlag(flag){
-	window.location.replace("/gestione/utenti.php?flag="+flag);
+	window.location.replace("/gestione/edifici.php?flag="+flag);
 }
+
+function addFiledMappale() {
+	let field = document.createElement('input');
+	field.name = 'mappNewEd'+mappaliNewEdCount;
+	field.placeholder = mappaliNewEdCount+'° mappale';
+	field.type = 'text';
+	field.pattern = '\\d{1,4}';
+	field.onKeyUp = function(){
+		console.log('stoca');
+	}
+	let div = document.createElement('div');
+	div.appendChild(field);
+	$('#mappali-new-ed').append(div);
+	mappaliNewEdCount++;
+}
+
+function submitNewEdificio() {
+	//TODO
+}
+
+
+
--- gestione/edifici.php
+++ gestione/edifici.php
@@ -24,59 +24,52 @@
 	<title>Gestione edifici</title>
 	<link rel="stylesheet" href="style_gestione.css">
 	<script type="text/javascript" src="/lib/jquery-3.3.1.min.js"></script>
+	<style type="text/css">
+	   #mappali-new-ed input{
+	       /*display: block;*/
+	   }
+	</style>
 </head>
 <body>
-	<div>
-		<form method="post">
-    		<input type="number" name="foglio" placeholder="Foglio..." value="<?= $_POST['foglio']??'' ?>">
-    		<input type="number" name="mappale" placeholder="Mappale..." value="<?= $_POST['mappale']??'' ?>">
-    		<input type="submit" value="Cerca">
-    	</form>
-	</div>
-
-
+	
 	<!-- Sidebar -->
     <div class="w3-sidebar w3-light-grey w3-bar-block" style="width:17%">
-      <p onclick="reloadPageWithFlag('richieste')" class="w3-bar-item w3-button">Richieste di<br>attivazione</p>
-      <p onclick="reloadPageWithFlag('gestione')" class="w3-bar-item w3-button">Gestione<br>accounts</p>
+      <p onclick="reloadPageWithFlag('vis-mod')" class="w3-bar-item w3-button">Visualizza/<br>modifica</p>
+      <p onclick="reloadPageWithFlag('new-ed')" class="w3-bar-item w3-button">Nuovo<br>edificio</p>
     </div>
     
     <!-- Page Content -->
     <div id="pageContent" style="margin-left:17%">
         
-        <div id="richieste" class="content">
-        	<div class="w3-container w3-teal"><h1>Account inattivi</h1></div>
-            
+        <div id="vis-mod" class="content">
+        	<div class="w3-container w3-teal"><h1>Visualizza/modifica edifci</h1></div>
             <div class="w3-container">
-                <?php 
-                foreach ($inattivi as $account) 
-                    echo "<div class=\"row\">
-                                <p>$account[Email]</p>
-                                <button onclick=\"activate('$account[Email]', this);\">Attiva</button>
-                              </div>";
-                ?>
+            	<form method="post">
+            		<input type="number" name="foglio" placeholder="Foglio..." value="<?= $_POST['foglio']??'' ?>">
+            		<input type="number" name="mappale" placeholder="Mappale..." value="<?= $_POST['mappale']??'' ?>">
+            		<input type="submit" value="Cerca">
+            	</form>
             </div>
         </div>
         
-        <div id="gestione" class="content" style="display: none;">
-        	<div class="w3-container w3-teal"><h1>Gestione accounts</h1></div>
-            
+        <div id="new-ed" class="content" style="display: none;">
+        	<div class="w3-container w3-teal"><h1>Nuovo edificio</h1></div>
             <div class="w3-container">
-                <?php 
-                    foreach ($attivi as $account) 
-                        echo "<div class=\"row\">
-                                    <p>$account[Email]</p>
-                                    <button onclick=\"deactivate('$account[Email]', this);\">Disattiva</button>
-                                    <button onclick=\"changeType('$account[Email]', '".($account['Type']=='ADMIN'?'USER':'ADMIN')."', this);\">".($account['Type']=='ADMIN'?'Declassa ad utente':'Promuovi ad admin')."</button>
-                                    <button onclick=\"deleteAccount('$account[Email]', this);\" style=\"background-color:red;\">Elimina</button>
-                                 </div>";
-                ?>
+            	<form method="post">
+            		<input type="number" name="foglio" placeholder="Foglio...">
+            		<div id="mappali-new-ed"></div>
+            		<button type="button" onclick="addFiledMappale();">+</button>
+            		<!-- ✔ -->
+            		<input type="button" onclick="submitNewEdificio();" value="CREA NUOVO EDIFICIO">
+            	</form>
             </div>
         </div>
         
     </div>
     
     <script type="text/javascript" src="/lib/misc.js"></script>
-    <script type="text/javascript" src="edifici.js"></script>
+	<script type="text/javascript" src="edifici.js"></script>
+    <script type="text/javascript">addFiledMappale();</script>
+    
 </body>
 </html>
